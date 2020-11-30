920107cad13b2b0022638433ea15462269bb2f2b
TobiasBms <tobi8678@edu.ucl.dk>
2020-11-30 15:33:57 +0100

    setup initial component for accordion

---
+++ src/components/BaseAccordion.vue
@@ -0,0 +1,26 @@
+<template>
+  <details>
+    <summary>{{summary}}</summary>
+    <p>
+      {{ description }}
+    </p>
+  </details>
+</template>
+
+<script>
+export default {
+  name: 'BaseAccordion',
+  props: {
+    description: {
+      type: String,
+      default: 'description',
+    },
+    summary:{
+      type: String,
+      default: 'default summary'
+    }
+  },
+}
+</script>
+
+<style></style>
